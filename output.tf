39c002fec51e69db4f2a35afeff711fd779266c7
Denny Alquinta <dralquinta@google.com>
2024-10-03 09:48:10 -0300

    Adding mount output

--- output.tf
+++ output.tf
@@ -5,3 +5,19 @@ output "disk_self_link" {
 output "disk_name" {
   value = google_compute_disk.disk.name
 }
+
+# Output the mount command
+output "mount_command" {
+  value = <<-EOT
+    #!/bin/bash
+    DISK_NAME="/dev/disk/by-id/google-${google_compute_disk.disk.name}"
+    MOUNT_POINT="/mnt/disks/${google_compute_disk.disk.name}"
+
+    # Create the mount point and mount the disk
+    sudo mkdir -p ${MOUNT_POINT}
+    sudo mount -o discard,defaults ${DISK_NAME} ${MOUNT_POINT}
+
+    # Add the disk to /etc/fstab for auto-mount on reboot
+    echo "${DISK_NAME} ${MOUNT_POINT} ext4 discard,defaults 1 1" | sudo tee -a /etc/fstab
+  EOT
+}
